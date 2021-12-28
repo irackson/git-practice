eed2e0b3c6066399b0f759de0f0867e383593c46
irackson <nasirus@protonmail.com>
2021-12-27 22:13:08 -0500

    update

---
+++ gitScripts.ts
@@ -0,0 +1,16 @@
+const util = require('util');
+const exec = require('child_process').exec;
+
+const command = `git add . && git commit -m "update" && git push`;
+
+const child = exec(
+	command,
+	function (error: string, stdout: string, stderr: string) {
+		console.log('stdout: ' + stdout);
+		console.log('stderr: ' + stderr);
+
+		if (error !== null) {
+			console.log('exec error: ' + error);
+		}
+	},
+);
