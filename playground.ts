8c4a1d71211eb8440887bc44c2b12792b359571b
irackson <nasirus@protonmail.com>
2023-01-28 10:29:26 -0500

    add new stuff

--- playground.ts
+++ playground.ts
@@ -1,4 +1,6 @@
 const files = ['gitScripts.ts', 'playground.ts', 'test.ts'];
 
-const msg = ['autoSync changes:'].concat(files).join('\n');
-console.log(msg);
+// const msg = ['autoSync changes:'].concat(files).join('\n');
+// console.log(msg);
+
+const newStuff = files.map((file) => `* ${file}`).join('\n');
